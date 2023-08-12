40a846ee32993d5b8472942cfd6084af72a65e94
Emilio Toledo <github@emiliotoledo.me>
2023-08-12 16:10:27 -0500

    feat(core): initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import { Config } from "tailwindcss";
+
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+
+const config: Config = {
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+
+	theme: {
+		extend: {},
+	},
+
+	plugins: [forms, typography],
+};
+
+export default config;
