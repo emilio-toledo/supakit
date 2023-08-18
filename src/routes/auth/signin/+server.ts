6f2b5092c394ed466875e25cf20efcab487b1e0d
Emilio Toledo <github@emiliotoledo.me>
2023-08-18 00:39:22 -0500

    feat(auth): add signin route

---
+++ src/routes/auth/signin/+server.ts
@@ -0,0 +1,19 @@
+import { error, json } from "@sveltejs/kit";
+import type { RequestHandler } from "./$types";
+import parseFormData from "$lib/utils/parseFormData";
+import type UserCredentials from "$lib/schemas/userCredentials";
+
+export const POST = (async ({ request, locals }) => {
+	const data = await request.formData().catch(() => {
+		throw error(400, "Form data missing in request.");
+	});
+
+	const { email, password } = parseFormData<UserCredentials>(data);
+
+	const authResponse = await locals.supabase.auth.signInWithPassword({
+		email,
+		password,
+	});
+
+	return json(authResponse);
+}) satisfies RequestHandler;
